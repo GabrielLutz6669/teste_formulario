982f99d20af077d9963c862b3b20b7df69213c1a
Gabriel Lutz <gabriellutzgp@gmail.com>
2023-04-21 10:49:39 -0300

    Initial commit

---
+++ processa_dados.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+    if (isset($_POST["acao"])){
+    echo "<script>alert('Formulário Foi enviado pelo método POST')</script>";
+    }
+?>
+
+</body>
+</html>
+
